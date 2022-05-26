13dbe026d5b8723c68b2b0fd804204a894488d3c
Rian <riansouza0@gmail.com>
2022-05-26 00:59:06 -0300

    pokemons-abilites-finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="home">
-    <h1>Olá mundo</h1>
+    <h1>charizard</h1>
     <input type="text" name="" id="" v-model="valor" />
-    <button @click="pesquisar()">Clique aqui</button>
-    <img v-show="this.img" :src="this.img" alt="" />
-    <h1 v-show="this.name">{{ this.name }}</h1>
-    <h1
-      v-for="(ab, key) in ability"
-      :key="key"
-      :value="ability"
-      v-show="ability"
-    >
-      {{ ab.name }}
-    </h1>
+    <button @click="pesquisar(img)">Clique aqui</button>
+    <div>
+      <img v-show="img" :src="img" alt="" />
+    </div>
+    <p v-show="name">Nome {{ name }}</p>
+    <p v-show="weight">Peso: {{ weight }}kg</p>
+    <div v-show="ability">
+      Habilidades:
+      <p v-for="(ab, key) in ability" :key="key" v-show="ability">
+        {{ ab.name }}
+      </p>
+    </div>
+    <p v-show="type" v-for="(tp, key) in type" :key="key">
+      {{ tp.name }}
+    </p>
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
+import { prominent } from "color.js";
+import { getAverageColor } from "fast-average-color";
 
 export default {
   name: "HomeView",
@@ -28,6 +34,8 @@ export default {
       ability: null,
       weight: null,
       img: null,
+      weight: null,
+      type: null,
     };
   },
   methods: {
@@ -39,12 +47,18 @@ export default {
 
       //Passando os valores da api para as variáveis
       this.name = res.name;
-      this.ability = res.abilities.map((valor) => valor.ability);
+      this.ability = res.abilities.map((value) => value.ability);
+      this.type = res.types.map((value) => value.type);
       this.weight = res.weight;
       this.img = res.sprites.front_default;
+      this.weight = res.weight / 10;
+
+      const color = getAverageColor(res.sprites.front_default);
+      console.log(color);
     },
-    mostrarConsole() {
-      console.log(this.valor);
+    async pickColor(img) {
+      const color = await getAverageColor(img);
+      console.log(color);
     },
   },
   components: {
@@ -55,6 +69,7 @@ export default {
 
 <style scoped>
 .home {
+  min-height: 200px;
   border-style: solid;
   width: 480px;
   margin: 0 auto;
